c8ce993a01c90415c31ff7261478175cd904d8cb
Che Liu <cliu88@berkeley.edu>
2024-07-30 12:23:46 -0400

    modified send int on client side

--- FPGA_AWG_client.py
+++ FPGA_AWG_client.py
@@ -3,39 +3,49 @@ from client import *
 # a python based client 
 class FPGA_AWG_client(Client):
 
-    def __init__(self, host, port):
-        super().__init__(host, port)
+    def __init__(self):
+        super().__init__()
 
     
     def upload_waveform_cfg(self, wf_cfg_path, name):
         self.send_string("UPLOAD_WAVEFORM_CFG")
         self.send_string(name)
         self.send_file(wf_cfg_path)
+        self.receive_server_ack()
 
-
-    def upload_envelope_data(self, idata_path, qdata_path, i_name, q_name):
+    # Use this function to upload either i data or q data. 
+    def upload_envelope_data(self, data_path, name):
         self.send_string("UPLOAD_ENVELOPE_DATA")
-        self.send_string(i_name)
-        self.send_file(idata_path)
-        self.send_string(q_name)
-        self.send_file(qdata_path)
+        self.send_string(name)
+        self.send_file(data_path)
+        self.receive_server_ack()
+
 
     def upload_program(self, prog_cfg_path, name):
         self.send_string("UPLOAD_PROGRAM")
         self.send_string(name)
         self.send_file(prog_cfg_path)
+        self.receive_server_ack()
+
 
     def delete_waveform_cfg(self, name):
         self.send_string("DELETE_WAVEFORM_CFG")
         self.send_string(name)
+        self.receive_server_ack()
+
 
     def delete_envelope_data(self, name):
         self.send_string("DELETE_ENVELOPE_DATA")
         self.send_string(name)
+        self.receive_server_ack()
+
+
 
     def delete_program(self, name):
         self.send_string("DELETE_PROGRAM")
         self.send_string(name)
+        self.receive_server_ack()
+
 
     def start_program(self, name):
         self.send_string("START_PROGRAM")
--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket 
 import os
+import struct 
 
 
 class Client():
@@ -10,16 +11,6 @@ class Client():
         self.host = None        # 192.168.0.234 by default
         self.port = None        # 8080 by default
 
-    
-    # To be deleted
-    # def connect_and_send_file(self, file_to_send):
-    #     try:
-    #         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-    #             client_socket.connect((self.host, self.port))
-    #             print(f"Connected to server {self.host} on port {self.port}")
-    #             self.send_file(file_to_send, client_socket) 
-    #     except Exception as e:
-    #         print(f"Error connecting to server: {e}")
     
     def connect(self, host, port):
         try:
@@ -40,9 +31,12 @@ class Client():
     def _utf8len(self, s):
         return len(s.encode('utf-8'))
 
+
     def send_int(self, n):
-        val = str(n) + '\n'
-        self.client_socket.sendall(val.encode())
+        # Pack the integer as a 4-byte big-endian format
+        val = struct.pack('!I', n)
+        self.client_socket.sendall(val)
+
 
 
     # send a single string s to server 
@@ -51,20 +45,17 @@ class Client():
             raise Exception("Client not connected.")
         
         try:
-            self.send_int(self._utf8len(s), s)        # send the length of words in bytes
+            self.send_int(self._utf8len(s))        # send the length of words in bytes
             self.client_socket.sendall(s.encode())
         except Exception as e:
             print(f"Error sending string: {e}")   
 
-        # response = self.client_socket.recv(4096).decode()
-        # print(f"Server response: {response}")
-    
-        
+
     def send_file(self, filename):
         file_size = os.stat(filename).st_size
         try:
-            self.send_int(file_size, self.client_socket)        # send the size of the file
-            self.send_int(len(filename), self.client_socket)    # send the size of filename in bytes
+            self.send_int(file_size)        # send the size of the file
+            self.send_int(len(filename))    # send the size of filename in bytes
             self.client_socket.sendall(filename.encode())  # send the file name first
             #continuously read and send the file content until EOF
             with open(filename, 'rb') as file:
@@ -74,13 +65,14 @@ class Client():
                         break
                     self.client_socket.sendall(bytes_read)
             print("File has been sent.")
-            # wait for acknolwdgement
-            # this line runs forever waiting for the server to send stuff
-            # this line will only block if there's nothing in the socket buffer 
-            ack = self.client_socket.recv(Client.buffer_size).decode()
-            print(f"Server acknowledgement: {ack}")
+
         except Exception as e:
             print(f"Error sending file: {e}")
+
+
+    def receive_server_ack(self):
+        ack = self.client_socket.recv(Client.buffer_size).decode()
+        print(f"Server acknowledgement: {ack}")
 
              
         
